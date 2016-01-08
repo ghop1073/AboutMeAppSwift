ce48265701fa8be3f37739702a70c74a4891f74b
ghop1073 <mhop1073@740s-pl-109.local>
2016-01-08 13:16:13 -0700

    idk why this is a commit again

---
+++ AboutMeAppSwift/BasketballViewController.swift
@@ -0,0 +1,27 @@
+//
+//  BasketballViewController.swift
+//  AboutMeAppSwift
+//
+//  Created by Hopkins, Michael on 1/8/16.
+//  Copyright © 2016 Hopkins, Michael. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class BasketballViewController : UIViewController
+{
+    @IBAction func changeToEnd(sender: UIButton)
+    {
+        performSegueWithIdentifier("toEnd", sender: sender)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
